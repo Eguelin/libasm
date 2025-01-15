509db2359479b28075d5f9f63712af9853d6bbcb
Eguelin <eguelin@student.42lyon.fr>
2025-01-15 15:21:23 +0100

    refactor: Simplify ft_strcpy by using a counter for indexing instead of pointer increments

--- srcs/ft_strcpy.s
+++ srcs/ft_strcpy.s
@@ -5,16 +5,16 @@ section .text
 
 	ft_strcpy:
 		mov rax, rdi ; return = dest
+		xor rcx, rcx ; rcx = 0
 
 	.loop:
-		mov	dl, [rsi] ; dl = *src
-		mov [rdi], dl ; *dest = *src
+		mov	dl, [rsi + rcx] ; dl = *src
+		mov [rdi + rcx], dl; *dest = *src
 
 		test dl, dl ; if (*src == 0)
 		jz .end
 
-		inc rdi ; dest++
-		inc rsi ; src++
+		inc rcx ; rcx++
 		jmp .loop
 
 	.end:
