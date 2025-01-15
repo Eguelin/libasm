a1048069f28a9f8395e74cacdf7dee75b6987bff
Eguelin <eguelin@student.42lyon.fr>
2025-01-15 10:52:45 +0100

    test: Add tests for list size and sorting functions

--- tester/srcs/test_bonus.c
+++ tester/srcs/test_bonus.c
@@ -6,7 +6,7 @@
 /*   By: eguelin <eguelin@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 15:41:05 by eguelin           #+#    #+#             */
-/*   Updated: 2025/01/14 15:32:39 by eguelin          ###   ########.fr       */
+/*   Updated: 2025/01/15 10:52:26 by eguelin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,87 @@ struct sigaction sa;
 
 void	test_atoi_base(void);
 void	test_list_push_front(void);
+void	test_list_size(void);
+void	test_list_sort(void);
+// void	test_list_remove_if(void);
 
 int main()
 {
 	sigemptyset(&sa.sa_mask);
 	test_atoi_base();
 	test_list_push_front();
+	test_list_size();
+	test_list_sort();
+	// test_list_remove_if();
 	return (0);
+}
+
+t_list	*list_push_front_c(t_list **begin_list, void *data)
+{
+	t_list	*new;
+
+	if (!(new = (t_list *)malloc(sizeof(t_list))))
+		return (NULL);
+	new->data = data;
+	new->next = *begin_list;
+	*begin_list = new;
+	return (new);
+}
+
+void	list_clear(t_list **begin_list)
+{
+	t_list	*tmp;
+
+	while (*begin_list)
+	{
+		tmp = *begin_list;
+		*begin_list = (*begin_list)->next;
+		free(tmp);
+	}
+}
+
+int	list_is_sort(t_list *begin_list, int (*cmp)())
+{
+	t_list	*tmp;
+
+	tmp = begin_list;
+	while (tmp && tmp->next)
+	{
+		if (cmp(tmp->data, tmp->next->data) > 0)
+			return (0);
+		tmp = tmp->next;
+	}
+	return (1);
+}
+
+int	list_size(t_list *begin_list)
+{
+	int		size;
+	t_list	*tmp;
+
+	size = 0;
+	tmp = begin_list;
+	while (tmp)
+	{
+		size++;
+		tmp = tmp->next;
+	}
+	return (size);
+}
+
+void	print_list(char *format, t_list *list)
+{
+	t_list	*tmp;
+
+	tmp = list;
+	if (tmp)
+	{
+		while (tmp)
+		{
+			printf(format, (char *)tmp->data);
+			tmp = tmp->next;
+		}
+	}
 }
 
 void	test_atoi_base(void)
@@ -78,61 +152,112 @@ void	test_atoi_base(void)
 void	test_list_push_front(void)
 {
 	t_list	*list = NULL;
-	char	*tab[] = {"elem1", "elem2", "elem3"};
+	char	*tab[] = {"elem1", "elem2", "elem3", "elem4", "elem5", "elem6", "elem7", "elem8", "elem9", "elem10"};
 
 	// Classic tests
 	printf(PURPLE"\t--- ft_list_push_front ---\n"RESET);
-	for (int i = 0; i < 3; i++)
+	for (int i = 0; i < 10; i++)
 	{
 		ASSERT_EXPR_CONDITION(ft_list_push_front(&list, tab[i]),
-		list->data == tab[i] && (list->next == NULL || list->next->data == tab[i - 1]));
-		printf(BLUE"ft_list_push_front(&list, \"%s\")\n"RESET, tab[i]);
+		list->data == tab[i] && (list->next == NULL || (list->next->data == tab[i - 1] && list_size(list) == i + 1)));
+		printf(BLUE"ft_list_push_front(&list, \"%s\") = ", tab[i]);
+		print_list("[%s] ", list);
+		printf("\n"RESET);
 	}
-	while (list)
-	{
-		t_list	*tmp = list;
-
-		list = list->next;
-		free(tmp);
-	}
+	list_clear(&list);
 
 	// NULL tests
 	ASSERT_NO_SEGFAULT(ft_list_push_front(NULL, "elem1"));
 	printf(BLUE"ft_list_push_front(NULL, \"elem1\")\n"RESET);
 
-
 	return ;
 }
 
-	// printf("ft_atoi_base(\"-100\", \"01\") = %d\n", ft_atoi_base("-100", "01"));
-
-	// printf("ft_atoi_base(\"\t -2147483648\", \"0123456789\") = %d\n", ft_atoi_base("\t -2147483648", "0123456789"));
-
-	// printf("ft_list_size(NULL) = %d\n", ft_list_size(NULL));
-
-	// t_list	*list = NULL;
-
-	// ft_list_push_front(NULL, "elem1");
-
-	// ft_list_push_front(&list, "elem1");
-	// ft_list_push_front(&list, "elem3");
-	// ft_list_push_front(&list, "elem2");
-
-	// printf("list->data = %s\n", (char *)list->data);
-
-	// printf("ft_list_size(elem1) = %d\n", ft_list_size(list));
-
-	// ft_list_sort(NULL, &ft_strcmp);
-
-	// ft_list_sort(&list, &ft_strcmp);
+void	test_list_size(void)
+{
+	t_list	*list = NULL;
+	char	*tab[1] = {"elem1"};
+	char	*tab2[3] = {"elem1", "elem2", "elem3"};
+	char	*tab3[5] = {"elem1", "elem2", "elem3", "elem4", "elem5"};
+	char	*tab4[10] = {"elem1", "elem2", "elem3", "elem4", "elem5", "elem6", "elem7", "elem8", "elem9", "elem10"};
+	char	**all_tab[4] = {tab, tab2, tab3, tab4};
+	int		size;
+	int		size_tab[4] = {1, 3, 5, 10};
 
 
-	// printf("list->data = %s\n", (char *)list->data);
-	// printf("list->next->data = %s\n", (char *)list->next->data);
-	// printf("list->next->next->data = %s\n", (char *)list->next->next->data);
+	// Classic tests
+	printf(PURPLE"\t--- ft_list_size ---\n"RESET);
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < size_tab[i]; j++)
+			list_push_front_c(&list, all_tab[i][j]);
+		ASSERT_EXPR_CONDITION(size = ft_list_size(list), size == size_tab[i]);
+		printf(BLUE"ft_list_size(list) = %d (", size);
+		print_list("[%s] ", list);
+		printf(")\n"RESET);
+		list_clear(&list);
+	}
 
-	// ft_list_remove_if(&list, "elem3", &ft_strcmp, &free);
-	// ft_list_remove_if(&list, "elem1", &ft_strcmp, &free);
-	// ft_list_remove_if(&list, "elem2", &ft_strcmp, &free);
+	// NULL tests
+	ASSERT_EXPR_CONDITION(size = ft_list_size(NULL), size == 0);
+	printf(BLUE"ft_list_size(NULL) = %d\n"RESET, size);
+}
 
-	// printf("list = %p\n", list);
+
+void	test_list_sort(void)
+{
+	t_list	*list = NULL;
+	char	*tab[3][1] = {{"elem1"}, {"elem2"}, {"elem3"}};
+	char	*tab2[6][3] = {{"elem1", "elem3", "elem2"},
+						{"elem3", "elem2", "elem1"},
+						{"elem2", "elem1", "elem3"},
+						{"elem2", "elem3", "elem1"},
+						{"elem3", "elem1", "elem2"},
+						{"elem1", "elem2", "elem3"}};
+	char	*tab3[10][5] = {{"elem1", "elem3", "elem2", "elem4", "elem5"},
+						{"elem3", "elem2", "elem1", "elem5", "elem4"},
+						{"elem2", "elem1", "elem3", "elem4", "elem5"},
+						{"elem2", "elem3", "elem1", "elem5", "elem4"},
+						{"elem3", "elem1", "elem2", "elem4", "elem5"},
+						{"elem1", "elem2", "elem3", "elem4", "elem5"},
+						{"elem1", "elem3", "elem2", "elem5", "elem4"},
+						{"elem3", "elem2", "elem1", "elem4", "elem5"},
+						{"elem2", "elem1", "elem3", "elem5", "elem4"},
+						{"elem2", "elem3", "elem1", "elem4", "elem5"}};
+
+	// Classic tests
+	printf(PURPLE"\t--- ft_list_sort ---\n"RESET);
+	for (int i = 0; i < 3; i++)
+	{
+		list_push_front_c(&list, tab[i][0]);
+		ASSERT_EXPR_CONDITION(ft_list_sort(&list, &strcmp), list_is_sort(list, &strcmp) && list_size(list) == 1);
+		printf(BLUE"ft_list_sort(list, &strcmp) = ");
+		print_list("[%s] ", list);
+		printf("\n"RESET);
+		list_clear(&list);
+	}
+	for (int i = 0; i < 6; i++)
+	{
+		for (int j = 0; j < 3; j++)
+			list_push_front_c(&list, tab2[i][j]);
+		ASSERT_EXPR_CONDITION(ft_list_sort(&list, &strcmp), list_is_sort(list, &strcmp) && list_size(list) == 3);
+		printf(BLUE"ft_list_sort(list, &strcmp) = ");
+		print_list("[%s] ", list);
+		printf("\n"RESET);
+		list_clear(&list);
+	}
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 5; j++)
+			list_push_front_c(&list, tab3[i][j]);
+		ASSERT_EXPR_CONDITION(ft_list_sort(&list, &strcmp), list_is_sort(list, &strcmp) && list_size(list) == 5);
+		printf(BLUE"ft_list_sort(list, &strcmp) = ");
+		print_list("[%s] ", list);
+		printf("\n"RESET);
+		list_clear(&list);
+	}
+
+	// NULL tests
+	ASSERT_NO_SEGFAULT(ft_list_sort(NULL, &strcmp));
+	printf(BLUE"ft_list_sort(NULL, &strcmp)\n"RESET);
+}
